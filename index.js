f497d2b82f722085093f2bc8e91889690b86808e
steadfast-method-9892 <dong.jason86@gmail.com>
2020-08-30 02:43:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function happyHolidays(){
   {return "Happy holidays!"}
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return "Happy holidays, ${name}`!"
 }
 function happyCustomHolidaysTo(holiday, name) {
-  return "Happy ${holidays}, ${name}!"
+  return "Happy ${holidays}`, ${name}`!"
 }
 function holidaysCountdown(holiday, name) {
-  return "It's ${days} days until ${holidays}!"
+  return "It's ${days}` days until ${holidays}`!"
 }
