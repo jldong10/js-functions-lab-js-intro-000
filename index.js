ced51ef533ecca32ad28837eece269d5b5aacf36
steadfast-method-9892 <dong.jason86@gmail.com>
2020-08-30 02:48:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function happyHolidays(){
   {return "Happy holidays!"}
 }
 function happyHolidaysTo(name) {
+  var name = "you"
   return "Happy holidays, ${name}`!"
 }
 function happyCustomHolidaysTo(holiday, name) {
